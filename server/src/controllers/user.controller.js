f90e35665ef6ac6c2a6db37b524f9a9d3ddf1ad8
luuAG <duongvannam2001@gmail.com>
2022-03-28 16:44:25 +0700

    temp commit 28/3/22

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,11 +1,39 @@
+const mongoose = require('mongoose');
+const User = require("../models/user.model");
+
 module.exports = {
 	// [GET] /api/user/me/profile
 	getMyProfile(req, res, next) {
 		res.send('profile');
 	},
-	// [GET] /api/user/
+	// [GET] /api/user
 	getUserProfile(req, res, next) {
-		res.send('user profile');
+		let id = req.decoded.user_id;
+		User.findOne({_id: id}, {rooms:0, auth:0})
+			.lean() 
+			.then(data =>{			
+				res.status(200).json(data); 
+			})
+			.catch(err => {
+				res.status(500).json({status: 'error', message: 'Error at server'});
+			})	
 	},
-	// [PATCH]
+	// [PATCH] /api/user/edit
+	editUserProfile(req, res, next) {
+		let id = req.decoded.user_id;
+		let reqData = req.body;
+		User.findOneAndUpdate({_id: id}, reqData)
+			.then(data => {
+				res.status(200).json({status: 'success', message: "User has been edited."});
+			})
+			.catch(err => {
+				res.status(500).json({status: 'error', message: "Error at server."});
+			})
+	},
+	// [DELETE] /api/user/delete
+	softDeleteUser(req, res, next) {
+		let id = req.params.id;
+		User.findOne({_id: id})
+			.delete();
+	}
 };
