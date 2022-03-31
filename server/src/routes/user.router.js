af5b479185719c2a40a7127d4d4189e965bb819f
luuAG <duongvannam2001@gmail.com>
2022-03-31 17:10:50 +0700

    Nam commit 31/3, on User module

--- server/src/routes/user.router.js
+++ server/src/routes/user.router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
 
-// router.get('/me/profile', userController.getMyProfile);
+router.get('/me/profile', userController.getMyProfile);
 router.get('/', userController.getUserProfile);
 router.patch('/edit', userController.editUserProfile);
 router.delete('/delete', userController.softDeleteUser);
