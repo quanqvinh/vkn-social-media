193372b2b7f08b6becf95b55aba2a11ccf5b92cd
luuAG <duongvannam2001@gmail.com>
2022-04-06 21:59:49 +0700

    Nam - temp commit 6/4/2022

--- server/src/routes/user.router.js
+++ server/src/routes/user.router.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const userController = require('../controllers/user.controller');
 
 router.get('/me/profile', userController.getMyProfile);
-router.get('/', userController.getUserProfile);
-router.patch('/edit', userController.editUserProfile);
+router.get('/:id', userController.getUserProfile);
+router.patch('/edit/info', userController.editUserProfile);
+
 router.delete('/delete', userController.softDeleteUser);
+
 
 module.exports = router;
