9dfb70758ad03b83d72318c1bfb705a3c11670e5
quanqvinh <nguyenquanqvinh@gmail.com>
2022-04-28 13:34:21 +0700

    Fix send message

--- server/vkn/src/listeners/message.handler.js
+++ server/vkn/src/listeners/message.handler.js
@@ -18,8 +18,10 @@ async function validateRoom(roomId, user1, user2) {
 			}
 		}
 	]);
+	console.log(room);
 	if (!room) 
 		return false;
+	let chatMate = room[0].charMate;
 	if (chatMate[0].toString() === user1 && chatMate[1].toString() === user2)
 		return true;
 	if (chatMate[1].toString() === user1 && chatMate[0].toString() === user2)
@@ -32,21 +34,23 @@ module.exports = (io, socket) => {
 		try {
 			let { username, userId, roomId, content } = payload;
 
-			if (!validateRoom(roomId, userId, socket.handshake.auth.userId))
-				throw Error('Room error');
+			// if (!validateRoom(roomId, userId, socket.handshake.auth.userId))
+			// 	throw Error('Room error');
 
 			let message = new Message({
 				sendBy: socket.handshake.auth.username,
 				content
 			});
-			
-			io.to(username).emit('chat:print_message', {
-				roomId,
-				message: message._doc
-			});
 
 			await Room.updateOne({ _id: roomId }, {
 				$push: { messages: message }
+			});
+			
+			io.to(username).emit('chat:print_message', {
+				roomId,
+				userId,
+				username: socket.handshake.auth.username,
+				message: message._doc
 			});
 		}
 		catch (error) {
@@ -77,6 +81,8 @@ module.exports = (io, socket) => {
 
 			io.to(username).emit('chat:print_image', {
 				roomId,
+				userId,
+				username: socket.handshake.auth.username,
 				message: message._doc
 			})
 
