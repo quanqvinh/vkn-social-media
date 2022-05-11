f460334ca321841b066da32e1df6fe96153cfea7
quanqvinh <nguyenquanqvinh@gmail.com>
2022-05-12 03:30:13 +0700

    Deploy heroku

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -2,11 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect(stringConnection) {
     try {
-        await mongoose.connect(stringConnection, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-        });
+        await mongoose.connect(stringConnection);
         console.log('Database connected!');
     } catch {
         console.log('Database connection failed!');
