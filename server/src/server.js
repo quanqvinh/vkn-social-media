6bba356020cc409876d8490a7dea6be3ce874a53
quanqvinh <nguyenquanqvinh@gmail.com>
2022-05-12 02:09:29 +0700

    Config dotenv

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ silent: process.env.NODE_ENV === 'production' });
 
 const db = require('./config/database');
 db.connect(process.env.STRING_CONNECTION);
