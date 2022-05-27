83088b6f9604dec345b2689c5e1562d0fc383428
quanqvinh <nguyenquanqvinh@gmail.com>
2022-05-27 18:48:38 +0700

    Fix

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
 api-user: npm run api-user
-api-admin: npm run api-admin
-web: npm run web
--- server/src/apis/user/server.js
+++ server/src/apis/user/server.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
-console.log(process.env.NODE_ENV);
+
 const db = require('../../config/database');
 db.connect(process.env.STRING_CONNECTION);
 
